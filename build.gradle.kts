aa49e238e73a939788299e0add8fb5eef2e598a7
Maxim Kazakov <kazakov.java@gmail.com>
2022-03-05 13:39:03 +0300

    2_TASK Доработка по reified inline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.6.20-M1"
 }
 
 group = "com.otus.kotlin"
--- src/main/kotlin/com/otus/kotlin/Animal.kt
+++ src/main/kotlin/com/otus/kotlin/Animal.kt
@@ -18,23 +18,10 @@ abstract class AnimalDsl {
         children.add(animal)
     }
 
-//    operator fun Animal<*>.plus(animal: Animal<*>){
-////        children.add(animal)
-//        children.add(animal)
-//    }
-
-    // Наличие infix-функций
-//    infix fun add(animalDsl: AnimalDsl) {
-//        add(animalDsl.build())
-//    }
-
-//    operator fun Animal<*>.plus(animalDsl: AnimalDsl){
-//        add(animalDsl.build())
-//    }
-
     abstract fun build(): Animal<*>
     
-    operator fun <T : AnimalDsl> plus(animalDsl: T) {
+    inline operator fun <reified T : AnimalDsl> plus(animalDsl: T) {
+        println("Кто я? ${T::class.simpleName}")
         add(animalDsl.build())
     }
 
