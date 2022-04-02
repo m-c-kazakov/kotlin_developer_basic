f1df104c1e2cf4dc180f7a02d8adde56ae6d6190
Maxim Kazakov <kazakov.java@gmail.com>
2022-04-02 15:05:17 +0300

    3_TASK Доработки по дз

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,4 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.junit.jupiter:junit-jupiter:5.8.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-test:1.6.0")
 }
